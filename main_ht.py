49ac872993a4031751824769c5c5298d3bfb82ab
KGFILHAKLFHEB <162285171+KGFILHAKLFHEB@users.noreply.github.com>
2024-03-06 17:33:12 +0900

    Update and rename main_hft.py to main_ht.py

--- main_ht.py
+++ main_ht.py
@@ -151,7 +151,7 @@ def get_args_parser():
     # Dataset parameters
     parser.add_argument('--data-path', default='/datasets01/imagenet_full_size/061417/', type=str,
                         help='dataset path')
-    parser.add_argument('--data-set', default='IMNET', choices=['CIFAR', 'IMNET', 'INAT', 'INAT19', 'CUB', 'CUBATT', 'CUB11', 'CUB11SEG'],
+    parser.add_argument('--data-set', default='IMNET', choices=['CIFAR', 'IMNET', 'INAT', 'INAT19', 'CUB', 'CUBATT', 'CUB11ATT'],
                         type=str, help='Image Net dataset path')
     parser.add_argument('--inat-category', default='name',
                         choices=['kingdom', 'phylum', 'class', 'order', 'supercategory', 'family', 'genus', 'name'],
@@ -554,20 +554,6 @@ def main(args):
         )
 
         lr_scheduler.step(epoch)
-        
-        # if epoch%30 ==0:
-        #     if args.output_dir:
-        #         checkpoint_paths = [output_dir / f'checkpoint{epoch}.pth']
-        #         for checkpoint_path in checkpoint_paths:
-        #             utils.save_on_master({
-        #                 'model':model_without_ddp.state_dict(),
-        #                 'optimizer':optimizer.state_dict(),
-        #                 'lr_scheduler':lr_scheduler.state_dict(),
-        #                 'epoch':epoch,
-        #                 'model_ema':get_state_dict(model_ema),
-        #                 'scaler':loss_scaler.state_dict(),
-        #                 'args':args,
-        #             },checkpoint_path)
         
         if args.output_dir:
             checkpoint_paths = [output_dir / 'checkpoint.pth']
