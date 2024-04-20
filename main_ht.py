4408b371a93d202bf6ad71f6953ffe9b5fe91356
KGFILHAKLFHEB <162285171+KGFILHAKLFHEB@users.noreply.github.com>
2024-04-21 00:20:55 +0900

    Update main_ht.py

--- main_ht.py
+++ main_ht.py
@@ -151,7 +151,7 @@ def get_args_parser():
     # Dataset parameters
     parser.add_argument('--data-path', default='/datasets01/imagenet_full_size/061417/', type=str,
                         help='dataset path')
-    parser.add_argument('--data-set', default='IMNET', choices=['CIFAR', 'IMNET', 'INAT', 'INAT19', 'CUB', 'CUBATT', 'CUB11ATT'],
+    parser.add_argument('--data-set', default='IMNET', choices=['CIFAR', 'IMNET', 'INAT', 'INAT19', 'CUB', 'CUBATT', 'CUBGHA'],
                         type=str, help='Image Net dataset path')
     parser.add_argument('--inat-category', default='name',
                         choices=['kingdom', 'phylum', 'class', 'order', 'supercategory', 'family', 'genus', 'name'],
